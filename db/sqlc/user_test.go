8f4ee01d8f1edcf85cd1726bfb06127ef021912e
ulunnuha-h <ulunnuha.h@gmail.com>
2025-03-06 15:44:33 +0700

    feat: add user query and update sqlc

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/ulunnuha-h/simple_bank/util"
+)
+
+func CreateRandomUser(t *testing.T) User {
+	args := CreateUserParams{
+		Username: util.RandomOwner(),
+		HashedPassword: "secret",
+		FullName: util.RandomString(5),
+		Email: util.RandomEmail(),
+	}
+
+	user, err := testQuery.CreateUser(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, args.Email, user.Email)
+	require.Equal(t, args.FullName, user.FullName)
+	require.Equal(t, args.HashedPassword, user.HashedPassword)
+	require.Equal(t, args.Username, user.Username)
+
+	require.NotEmpty(t, user.CreatedAt)
+	require.NotEmpty(t, user.PasswordChangedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	CreateRandomUser(t)
+}
+
+func TestGetuser(t *testing.T) {
+	user := CreateRandomUser(t)
+	user2, err := testQuery.GetUser(context.Background(), user.Username)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user.Email, user2.Email)
+	require.Equal(t, user.FullName, user2.FullName)
+	require.Equal(t, user.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user.Username, user2.Username)
+	require.WithinDuration(t, user.CreatedAt, user2.CreatedAt, time.Second)
+	require.WithinDuration(t, user.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+}

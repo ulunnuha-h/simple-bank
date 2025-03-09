eebf6bdf6ea45a0a5ed6d8c10a57ae3c84090cff
ulunnuha-h <ulunnuha.h@gmail.com>
2025-03-09 16:37:09 +0700

    fix: load env on main test

--- api/main_test.go
+++ api/main_test.go
@@ -1,13 +1,20 @@
 package api
 
 import (
+	"log"
 	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ulunnuha-h/simple_bank/util"
 )
 
 func TestMain(m *testing.M) {
+	err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Failed to load .env file")
+	}
+
 	gin.SetMode(gin.TestMode)
 
 	os.Exit(m.Run())
